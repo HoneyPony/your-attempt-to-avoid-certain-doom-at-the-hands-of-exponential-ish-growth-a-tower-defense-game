18291978dc4fd16e88370cd9dfe57a038d9e8fd6
Honey Pony <honeyponygames@gmail.com>
2023-08-12 17:37:00 -0400

    Work on visuals... will need to see how performant they are in general

--- virus/Virus.gd
+++ virus/Virus.gd
@@ -5,6 +5,10 @@ var coord: Vector2 = Vector2.ZERO
 
 func _ready():
 	parent_vc = get_parent()
+	$Energy.material.set_shader_param("rng_source", Vector2(rand_range(0, 16), rand_range(0, 16)))
+
+	var overlay_rot_options = [0, TAU/4, TAU/2, 0.75 * TAU]
+	$Overlay.rotation = overlay_rot_options[randi() % 4]
 
 func destroy():
 	parent_vc.free_coord(coord)
@@ -15,7 +19,13 @@ func _physics_process(delta):
 	var target_rot = 0
 	var target_pos = coord * 32 # 4x pixels, plus 8 pixels wide
 	
+	# Jitter animation:
+	target_scale *= rand_range(0.9, 1.1)
+	target_rot += rand_range(-0.1 * TAU, 0.1 * TAU)
+	target_pos += polar2cartesian(rand_range(0, 4), rand_range(0, TAU))
+	
 	scale += (target_scale - scale) * 0.09
-	rotation += (target_rot - rotation) * 0.09
+	
+	rotation = lerp_angle(rotation, target_rot, 0.09)
 	position += (target_pos - position) * 0.09
 	pass
