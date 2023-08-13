a6997a1400fe515bd636d3e5489cecb55619ecf6
Honey Pony <honeyponygames@gmail.com>
2023-08-13 19:54:39 -0400

    Implement basic drag system; need to consider aspect ratio more

--- towers/BasicGun.gd
+++ towers/BasicGun.gd
@@ -10,9 +10,55 @@ func fire():
 	var bullet = GS.BasicBullet.instance()
 	bullet.position = position
 	get_parent().add_child(bullet)
+	
+
 
 func _physics_process(delta):
 	fire_timer -= delta
 	if fire_timer <= 0:
 		fire_timer = fire_timer_max
 		fire()
+		
+	#if Input.is_mouse_button_pressed(BUTTON_LEFT)
+
+
+enum DragState {
+	NOT_DRAGGING,
+	DRAG_MOUSE,
+	DRAG_TOUCH
+}
+
+# Keeps track of how the current gun is being dragged, if at all.
+# Dragging with the mouse and with the touch screen are considered
+# mutually exclusive for simplicity.
+var drag_state = DragState.NOT_DRAGGING
+
+# Only activate dragging states through inputs that occur on this Area2D.
+func _on_BasicGun_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				drag_state = DragState.DRAG_MOUSE
+	
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			drag_state = DragState.DRAG_TOUCH
+				
+				
+func _input(event):
+	# Dragging states may be deactivated by any input event.
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			if drag_state == DragState.DRAG_MOUSE:
+				drag_state = DragState.NOT_DRAGGING
+	
+	if event is InputEventScreenTouch:
+		if drag_state == DragState.DRAG_TOUCH and not event.pressed:
+				drag_state = DragState.NOT_DRAGGING
+	
+	if drag_state == DragState.DRAG_MOUSE:
+		if event is InputEventMouseMotion:
+			position += event.relative
+	elif drag_state == DragState.DRAG_TOUCH:
+		if event is InputEventScreenDrag:
+			position += event.relative
