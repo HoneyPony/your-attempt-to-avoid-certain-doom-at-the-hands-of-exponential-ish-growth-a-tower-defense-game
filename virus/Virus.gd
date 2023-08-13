6435598d7b96a4ad4e66c5e930a885926eaecfca
Honey Pony <honeyponygames@gmail.com>
2023-08-12 20:20:13 -0400

    Various tweaks to see how they might affect the shape

--- virus/Virus.gd
+++ virus/Virus.gd
@@ -6,6 +6,20 @@ var coord: Vector2 = Vector2.ZERO
 var spawn_timer = 1.0
 var direction_preference = null
 
+var generation = 1
+
+# Only attempt to spawn a certain number of times before giving up forever.
+# (disabled for now)
+# var spawns_left = 15
+
+# "Inherited" traits:
+# Traits that intend to asymetrize the tree, by being inherited + mutated along the way.
+# Speed: how fast we make children. Some viruses will be slower or faster.
+var speed = 1.0
+
+# Strength: the highest generation allowed of this virus.
+var strength = 12
+
 func _ready():
 	parent_vc = get_parent()
 	$Energy.material.set_shader_param("rng_source", Vector2(rand_range(0, 16), rand_range(0, 16)))
@@ -21,8 +35,9 @@ func _physics_process(delta):
 	spawn_timer -= delta
 	if spawn_timer < 0:
 		try_spawns()
+		#if spawns_left > 0:
 		# Reset timer
-		spawn_timer = parent_vc.spawn_timer_max * rand_range(0.95, 1.05)
+		spawn_timer = parent_vc.spawn_timer_max * rand_range(0.95, 1.05) * speed
 	
 	# Animate the spawn/jitter effects
 	animate()
@@ -62,10 +77,20 @@ func spawn(coord, dir):
 	# Hue shift... shows child-parent relationships, as well, which might
 	# be cool
 	var hue = color.h
-	hue += rand_range(0.01, 0.04)
+	hue += rand_range(0.04, 0.08)
 	while hue > 1.0:
 		hue -= 1.0
 	v.get_node("Energy").modulate = Color.from_hsv(hue, color.s, color.v)
+	
+	# Keep track of generation
+	v.generation = generation + 1
+	
+	# Genetic mutation
+	v.speed = speed * rand_range(0.95, 1 / 0.95)
+	v.speed = clamp(v.speed, 0.7, 1.4)
+	
+	v.strength = strength + rand_range(-2.5, 2.5)
+	v.strength = clamp(v.strength, 8, 18)
 	
 	parent_vc.add_child(v)
 	
@@ -74,13 +99,23 @@ func try_spawn(direction):
 	if new_coord in parent_vc.used_coordinates:
 		return false
 		
+	if new_coord in parent_vc.avoid_coordinates:
+		return false
+		
 	spawn(new_coord, direction)
 	return true
 	
 func try_spawns():
+	if generation >= strength:
+		return
+	
 	var coord_deltas = [Vector2(0, 1), Vector2(0, -1), Vector2(1, 0), Vector2(-1, 0)]
 	
-	if direction_preference != null:
+	# In order to encourage direction preferences to be more interesting,
+	# we only look at them if they aren't centered on the origin.
+	var check_direction_preference = (coord.x != 0) and (coord.y != 0)
+	
+	if direction_preference != null and check_direction_preference:
 		# Some percent of the time, try to grow in the same direction.
 		if randf() < 0.8:
 			# Temporary variable to save value
@@ -94,5 +129,6 @@ func try_spawns():
 	# Try a couple times before giving up.
 	for i in range(0, 3):
 		var dir = coord_deltas[randi() % 4]
+		#spawns_left -= 1
 		if try_spawn(dir):
 			return
--- virus/VirusCollection.gd
+++ virus/VirusCollection.gd
@@ -3,6 +3,22 @@ class_name VirusCollection
 
 var used_coordinates = { Vector2(0, 0): true }
 
+# coordinates to be avoided - so that the shapes might be more interesting..?
+var avoid_coordinates = {}
+
+func random_avoid_value():
+	# Never generate 0
+	var v = 1 + (randi() % 15)
+	if randf() < 0.5:
+		v = -v
+	return v
+	
+
+func _ready():
+	for i in range(0, 60):
+		var a = Vector2(random_avoid_value(), random_avoid_value())
+		avoid_coordinates[a] = true
+
 # Controls the spawn timer of all child viruses.
 var spawn_timer_max = 2.0
 
