49c01bd7d494bde0c4632acd8d19ee6ac1834083
Honey Pony <honeyponygames@gmail.com>
2023-08-13 20:00:58 -0400

    Aspect ratio tweaks (16:9), add fullscreen shortcut(shift+enter or F11)

--- GS.gd
+++ GS.gd
@@ -3,3 +3,7 @@ extends Node
 var Virus = preload("res://virus/Virus.tscn")
 
 var BasicBullet = preload("res://towers/BasicBullet.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
