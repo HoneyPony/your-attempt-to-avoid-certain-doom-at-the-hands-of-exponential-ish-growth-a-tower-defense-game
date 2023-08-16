df37ce4408c0903775711d49af808d35c4d7be32
Honey Pony <honeyponygames@gmail.com>
2023-08-16 16:08:53 -0400

    Tweaks to ShipBuyer -- swap Info and Buy buttons We want the Buy button to be far away from the close/open shop button.

--- ui/ShipBuyer.gd
+++ ui/ShipBuyer.gd
@@ -5,7 +5,7 @@ export var cost: int = 50
 onready var buy_button = $BuyButton
 
 func _ready():
-	$CostLabel.text = str("Cost: $", cost)
+	$CostLabel.text = str(cost)
 	
 func _process(delta):
 	buy_button.disabled = (GS.money < cost)
