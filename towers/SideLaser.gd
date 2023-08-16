7a8d6f22e162d43605b8fd7f3657b6d934c440b1
Honey Pony <honeyponygames@gmail.com>
2023-08-16 01:44:55 -0400

    Work on side laser bullets... still kind of problematic

--- towers/SideLaser.gd
+++ towers/SideLaser.gd
@@ -7,6 +7,11 @@ var fire_timer_max = 0.3
 
 onready var bullet_spawn_point = $BulletSpawnPoint
 
+var prev_bullet = null
+
+# Where bullets will connect to with line drawing.
+onready var front = $Front
+
 func _ready():
 	fire_timer = fire_timer_max
 	
@@ -19,6 +24,19 @@ func fire():
 	# for now.
 	bullet.velocity = Vector2(-360, 0)
 	get_parent().add_child(bullet)
+	
+	# By default, the "next" bullet in the chain is just ourselves. We need to
+	# have a "front" member for this to work.
+	bullet.next = self
+	
+	# Link the bullets together so they can render
+	bullet.prev = prev_bullet
+	if is_instance_valid(prev_bullet):
+		# Turn off the straight tail that it had
+		prev_bullet.next = bullet
+	
+	# Update this var so we can use it next time
+	prev_bullet = bullet
 
 func _physics_process(delta):
 	# The side laser never moves from.. well.. the side.
