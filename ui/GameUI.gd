6bfcf37bc55e553ef0e372177294a1a05a698d80
Honey Pony <honeyponygames@gmail.com>
2023-08-16 15:53:54 -0400

    Work on moving shop UI to the bottom

--- ui/GameUI.gd
+++ ui/GameUI.gd
@@ -3,6 +3,18 @@ extends Control
 onready var money_counter = $Shop/Money
 onready var lives_counter = $Shop/Lives
 
+onready var shop_anim = $Shop/AnimationPlayer
+
+var shop_open = true
+
 func _process(delta):
 	money_counter.text = str("$ ", GS.money)
 	lives_counter.text = str(GS.lives)
+
+
+func _on_CloseButton_pressed():
+	if shop_open:
+		shop_anim.play("HideShop")
+	else:
+		shop_anim.play("ShowShop")
+	shop_open = not shop_open
