86cc47325825fb4d64abaf26c661479c0e150381
Honey Pony <honeyponygames@gmail.com>
2023-08-16 23:54:00 -0400

    [NOT YET RUN] create tool script for disabling focus on all buttons

---
+++ TOOL_RemoveAllButtonFocus.gd
@@ -0,0 +1,20 @@
+tool
+extends Node2D
+
+export var flag = false
+
+func remove_focus_on_all_children(node: Node):
+	if node is Button:
+		var b: Button = node
+		b.focus_mode = Control.FOCUS_NONE
+	
+	for child in node.get_children():
+		remove_focus_on_all_children(child)
+
+func go():
+	remove_focus_on_all_children(self)
+
+func _process(delta):
+	if flag:
+		flag = false
+		go()
