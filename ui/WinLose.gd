55cfb869dc75a54a6a5a2b69f0d8fb13ef16baf4
Honey Pony <honeyponygames@gmail.com>
2023-08-18 20:28:32 -0400

    [x10 virus speed for debug] Fix Lose screen, add pausing to win/lose

--- ui/WinLose.gd
+++ ui/WinLose.gd
@@ -13,13 +13,22 @@ func lose():
 
 
 func _on_StartOver_pressed():
+	get_tree().paused = false
+	# We MUST reset game state.
+	GS.reset_game_state()
 	SceneTransition.transition_to(GS.Game)
 
 
 func _on_QuitToMenu_pressed():
+	get_tree().paused = false
 	SceneTransition.transition_to(GS.MainMenu)
 
 
 func _on_FreePlay_pressed():
+	get_tree().paused = false
 	GS.game_state = GS.GameState.FREEPLAY
 	$WinScreen/AnimationPlayer.play("Close")
+	
+func pause():
+	# Make sure nothing funny happens while the menu is open.
+	get_tree().paused = true
