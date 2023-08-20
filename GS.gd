01decaa00520449d26aa636f124eed6e2b079d1e
Honey Pony <honeyponygames@gmail.com>
2023-08-20 00:25:33 -0400

    Changes to kinetic sound; progress on new music

--- GS.gd
+++ GS.gd
@@ -423,8 +423,8 @@ func reset_game_state():
 	ship_buyers = [null, null, null, null, null, null]
 	
 	# DEBUG MONEY:
-	#money = 54000
-	#total_money = 54000
+	money = 7000
+	total_money = 7000
 	
 	reset_timers()
 	
